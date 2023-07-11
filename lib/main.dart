5dee63e693186a9bf8b1de13622806cd62dad7fe
Vaibhav Lokhande <133968314+abhishekmalhotra0312@users.noreply.github.com>
2023-07-11 19:25:52 +0530

    Added adress confirm

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:sar/palette.dart';
-import 'package:sar/pages/welcome.dart';
+// import 'package:sar/palette.dart';
+import 'package:sar/pages/addressConfirm.dart';
 
 void main() {
   runApp(MyApp());
@@ -18,27 +18,21 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const Welcome(),
+      home: const addressConfirm(),
       theme: ThemeData(
         appBarTheme: const AppBarTheme(
             backgroundColor: Color.fromRGBO(247, 182, 26, 1),
             centerTitle: false),
-
-
-
         elevatedButtonTheme: ElevatedButtonThemeData(
           style: ElevatedButton.styleFrom(
             backgroundColor: const Color.fromRGBO(184, 28, 34, 1),
             padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
             side: const BorderSide(
-                width: 1, color: Color.fromRGBO( 251, 25, 41, 1)),
+                width: 1, color: Color.fromRGBO(251, 25, 41, 1)),
             shape:
                 RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
           ),
         ),
-
-
-     
       ),
     );
   }
